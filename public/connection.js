5cabd91fa0140ae672d79634ff72c2f425e142f6
GuinnessShep <108033341+GuinnessShep@users.noreply.github.com>
2023-03-14 20:39:54 -0700

    Update connection.js

--- public/connection.js
+++ public/connection.js
@@ -1,60 +1,95 @@
-/**
- * Wrapper for client-side TikTok connection over Socket.IO
- * With reconnect function.
- */
 class TikTokIOConnection {
-    constructor(backendUrl) {
-        this.socket = io(backendUrl);
+  constructor(backendUrl) {
+    this.backendUrl = backendUrl;
+    this.socket = null;
+    this.uniqueId = null;
+    this.options = null;
+    this.connected = false;
+
+    this.connect = this.connect.bind(this);
+    this.setUniqueId = this.setUniqueId.bind(this);
+
+    this.connect();
+  }
+
+  connect() {
+    return new Promise((resolve, reject) => {
+      this.socket = io(this.backendUrl);
+
+      this.socket.on('connect', () => {
+        console.info('Socket connected!');
+        this.connected = true;
+
+        // Reconnect to streamer if uniqueId already set
+        if (this.uniqueId) {
+          this.setUniqueId();
+        }
+
+        resolve();
+      });
+
+      this.socket.on('disconnect', () => {
+        console.warn('Socket disconnected!');
+        this.connected = false;
+      });
+
+      this.socket.on('streamEnd', () => {
+        console.warn('LIVE has ended!');
         this.uniqueId = null;
-        this.options = null;
+      });
 
-        this.socket.on('connect', () => {
-            console.info("Socket connected!");
+      this.socket.on('tiktokDisconnected', (errMsg) => {
+        console.warn(errMsg);
+        if (errMsg && errMsg.includes('LIVE has ended')) {
+          this.uniqueId = null;
+        }
+      });
 
-            // Reconnect to streamer if uniqueId already set
-            if (this.uniqueId) {
-                this.setUniqueId();
-            }
+      setTimeout(() => {
+        if (!this.connected) {
+          this.disconnect();
+          reject(new Error('Connection timeout'));
+        }
+      }, 15000);
+    });
+  }
+
+  setUniqueId() {
+    if (this.connected) {
+      this.socket.emit('setUniqueId', this.uniqueId, this.options);
+    }
+  }
+
+  reconnect() {
+    if (!this.connected) {
+      console.info('Attempting to reconnect...');
+      this.connect()
+        .then(() => {
+          console.info('Reconnected!');
         })
-
-        this.socket.on('disconnect', () => {
-            console.warn("Socket disconnected!");
-        })
-
-        this.socket.on('streamEnd', () => {
-            console.warn("LIVE has ended!");
-            this.uniqueId = null;
-        })
-
-        this.socket.on('tiktokDisconnected', (errMsg) => {
-            console.warn(errMsg);
-            if (errMsg && errMsg.includes('LIVE has ended')) {
-                this.uniqueId = null;
-            }
+        .catch((err) => {
+          console.warn(`Reconnection failed: ${err}`);
         });
     }
+  }
 
-    connect(uniqueId, options) {
-        this.uniqueId = uniqueId;
-        this.options = options || {};
-
-        this.setUniqueId();
-
-        return new Promise((resolve, reject) => {
-            this.socket.once('tiktokConnected', resolve);
-            this.socket.once('tiktokDisconnected', reject);
-
-            setTimeout(() => {
-                reject('Connection Timeout');
-            }, 15000)
-        })
+  disconnect() {
+    if (this.connected) {
+      console.info('Disconnecting...');
+      this.socket.disconnect();
+      this.uniqueId = null;
+      this.options = null;
+      this.connected = false;
     }
+  }
 
-    setUniqueId() {
-        this.socket.emit('setUniqueId', this.uniqueId, this.options);
+  on(eventName, eventHandler) {
+    if (this.connected) {
+      this.socket.on(eventName, eventHandler);
     }
+  }
 
-    on(eventName, eventHandler) {
-        this.socket.on(eventName, eventHandler);
-    }
+  get isConnected() {
+    return this.connected;
+  }
 }
